7da00d7b12ad251778ec3ca09cf273e9242c662a
davidw <david.pg18@gmail.com>
2021-01-27 15:37:25 +0800

    CH02-Ex04: Remove Sleep Test
    額外測試 Thread.Sleep(0); 和 沒用 Thread.Sleep() 是不同的結果

--- Examples/Ch02/Ex04_SharedState/Program.cs
+++ Examples/Ch02/Ex04_SharedState/Program.cs
@@ -21,7 +21,7 @@ namespace Ex04_SharedState
             var t1 = new Thread(AddToCart);
             var t2 = new Thread(AddToCart);
 
-            t1.Start(300); // 試試改傳入 0
+            t1.Start(0); // 試試改傳入 0
             t2.Start(100);
         }
 
@@ -34,8 +34,22 @@ namespace Ex04_SharedState
              * 由呼叫端傳入的 simulateDelay 參數指定，以便藉由改變
              * 此參數來觀察共享變數值的變化。
              */
-            Thread.Sleep((int)simulateDelay);
-            Console.WriteLine("Items in cart: {0}", itemCount);
+
+            var isTestSleep = true;
+            if (isTestSleep)
+            {
+                Thread.Sleep((int)simulateDelay);
+            }
+            else
+            {
+                // 額外測試 Thread.Sleep(0); 和 沒用 Thread.Sleep() 是不同的結果
+                if ((int)simulateDelay != 0)
+                {
+                    Thread.Sleep((int)simulateDelay);
+                }
+            }
+            
+            Console.WriteLine("Items in cart: {0}, TheadId: {1}", itemCount, Thread.CurrentThread.ManagedThreadId);
         }
     }
 }
