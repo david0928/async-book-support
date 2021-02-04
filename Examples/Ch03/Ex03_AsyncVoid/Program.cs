29c4d92f6fa254fea50652ce3ee1616456283aad
davidw <david.pg18@gmail.com>
2021-02-04 19:52:14 +0800

    Ex03_AsyncVoid - 加入 ThreadId

--- Examples/Ch03/Ex03_AsyncVoid/Program.cs
+++ Examples/Ch03/Ex03_AsyncVoid/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Ex03_AsyncVoid
@@ -13,13 +14,20 @@ namespace Ex03_AsyncVoid
             }
             catch (Exception ex) // 捕捉不到 TestVoidAsync 方法所拋出的異常!
             {
-                Console.WriteLine(ex);  
+                Console.WriteLine(ex);
             }
         }
 
         static async void TestVoidAsync()
         {
+            Console.WriteLine(Thread.CurrentThread.ManagedThreadId);
+
             await Task.Delay(0); // 只是為了讓此方法有用到 await 陳述式。
+            //await Task.Yield(); // 讓此方法有用到 await 陳述式，也可以改用 Task.Yield()，但使用 Task.Yield() 會更換 Thread
+
+            Console.WriteLine(Thread.CurrentThread.ManagedThreadId);
+
+
             throw new Exception("error");
         }
     }
